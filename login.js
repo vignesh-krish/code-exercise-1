5e56b5ff37d0e2d8e6f31ba31df14e0443094fd6
vignesh-krish <vigneshsk.jmc@gmail.com>
2020-07-07 10:19:36 +0530

    updated fund raiser and login

---
+++ login.js
@@ -0,0 +1,5 @@
+import { Tabs } from "./tabs";
+
+const tab1 = document.getElementById("tab1");
+
+Tabs(tab1);
--- script.js
+++ script.js
@@ -1,6 +1,2 @@
 import "./styles.scss";
-import { Tabs } from "./tabs";
-
-const tab1 = document.getElementById("tab1");
-
-Tabs(tab1);
+import "./fund.scss";
