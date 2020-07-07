25ac0ee1bdcc8737b649054aaeaa0072f7d4cf27
vignesh-krish <vigneshsk.jmc@gmail.com>
2020-07-07 11:46:30 +0530

    updated to append button

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
 import "./styles.scss";
 import "./fund.scss";
+
+function appendToBody(buttons) {
+  buttons.forEach(btn => {
+    document.getElementById("buttonContainer").appendChild(btn);
+    btn.addEventListener("click", function() {
+      logButtonIndexAndText(btn);
+    });
+  });
+}
+function logButtonIndexAndText(btn) {
+  const parentNode = btn.parentElement;
+  const nodes = Array.from(parentNode.children);
+  const index = nodes.indexOf(btn);
+  const btnText = btn.innerHTML;
+  console.log(`The ${btnText} button is at index ${index}`);
+}
+function createButtons(buttonText) {
+  let buttons = [];
+  for (let txt of buttonText) {
+    const btn = document.createElement("button");
+    btn.innerHTML = txt;
+    buttons.push(btn);
+  }
+  appendToBody(buttons);
+}
+
+const buttonTexts = ["Hello", "World", "Welcome", "Greetings", "Thank you"];
+createButtons(buttonTexts);
